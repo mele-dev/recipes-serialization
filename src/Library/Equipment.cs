0e17ac153f202635d973cffa733e54059c4998d6
JuanTanca <65884059+mele-dev@users.noreply.github.com>
2023-09-18 19:20:11 -0300

    Initial commit

---
+++ src/Library/Equipment.cs
@@ -0,0 +1,35 @@
+//-------------------------------------------------------------------------------
+// <copyright file="Equipment.cs" company="Universidad Católica del Uruguay">
+// Copyright (c) Programación II. Derechos reservados.
+// </copyright>
+//-------------------------------------------------------------------------------
+
+using System.Text.Json;
+using System.Text.Json.Serialization;
+
+namespace Recipies
+{
+    public class Equipment
+    {
+        [JsonConstructor]
+        public Equipment(string description, double hourlyCost)
+        {
+            this.Description = description;
+            this.HourlyCost = hourlyCost;
+        }
+
+        public Equipment(string json)
+        {
+            this.LoadFromJson(json);
+        }
+
+        public string Description { get; set; }
+
+        public double HourlyCost { get; set; }
+
+        public string LoadFromJson(string json)
+        {
+            return "";
+        }
+    }
+}
